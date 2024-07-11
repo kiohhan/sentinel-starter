4c9c2e8d0f82fa88848e6cfe4ef54e3a1c4ca2de
Kioh <kiohhan.dev@gmail.com>
2024-07-11 17:52:36 +0000

    update with mkdir

--- macos/docker-run.sh
+++ macos/docker-run.sh
@@ -7,7 +7,8 @@ node_tag="22.1-bullseye"
 ssh_file="id_rsa_personal"
 
 containerid=$(docker run -d -t node:$node_tag)
+docker exec $containerid mkdir -p /root/.ssh
 docker container cp ~/.ssh/$ssh_file $containerid:/root/.ssh/id_rsa
-docker exec $containerid mkdir /root/work
+docker exec $containerid mkdir -p /root/work
 docker exec $containerid git config --global user.email "kiohhan.dev@gmail.com"
 docker exec $containerid git config --global user.name "Kioh"
